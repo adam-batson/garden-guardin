4a871a947faa199ac6f3830d0bd42fd9699898d0
Adam Batson <tabatson@gmail.com>
2021-12-20 10:07:29 -0600

    Daily push

---
+++ src/scenes/Preload.js
@@ -0,0 +1,15 @@
+import Phaser from 'phaser';
+import garden from '../assets/images/garden.png';
+import 
+
+export default class Preload extends Phaser.Scene
+{
+    preload()
+    {
+        this.load.image('garden', garden);
+    }
+    create()
+    {
+        this.scene.start('title-screen');
+    }
+}
