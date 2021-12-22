d03c0eb86d4685991fa98d975c59a91ed3f5b2ee
Adam Batson <tabatson@gmail.com>
2021-12-21 21:37:37 -0600

    Daily commit

--- src/scenes/Preload.js
+++ src/scenes/Preload.js
@@ -1,16 +1,25 @@
 import Phaser from 'phaser';
-import * as images from '../assets/images/*.png';
+import garden from '../assets/images/garden.png';
+import spade from '../assets/images/Spade.png';
 import sprite from '../assets/images/Spade.json';
 
+// Parcel requires audio to be imported this way
+const bgmUrl = new URL('../assets/sounds/song18.mp3', import.meta.url);
+const hitUrl = new URL('../assets/sounds/hit35.mp3.flac', import.meta.url);
+     
 export default class Preload extends Phaser.Scene
 {
     preload()
-    {
-        this.load.image('garden', images);
-        this.load.aseprite('player', images.Spade, sprite);
+    {     
+        this.load.image('garden', garden);
+        this.load.aseprite('player', spade, sprite);
+        this.load.audio('bgm', `${bgmUrl}`);
+        this.load.audio('hit-sound', `${hitUrl}`);
     }
     create()
     {
+        this.anims.createFromAseprite('player');
+        console.log('loaded sprite anims')
         this.scene.start('title-screen');
     }
 }
--- src/scenes/TitleScreen.js
+++ src/scenes/TitleScreen.js
@@ -9,7 +9,14 @@ export default class TitleScreen extends Phaser.Scene
 
     create()
     {
-        this.add.text(400, 400, 'Press SPACE to start');
+        this.add.text(400, 250, "Garden Guardin'")
+            .setOrigin(0.5, 0.5)
+            .setFontFamily('game-font')
+            .setFontSize(40);
+        this.add.text(400, 400, 'Press SPACE to start')
+            .setOrigin(0.5, 0.5)
+            .setFontFamily('game-font')
+            .setFontSize(32);
 
         this.input.keyboard.once('keydown-SPACE', () => {
             this.scene.start('level-one');
